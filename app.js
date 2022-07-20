994fa1051c7e1277d72d4dfb575878e1ef126247
Marshall Buck <dev@marshallbuck.com>
2022-07-19 22:11:20 -0500

    doc strings added

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
 'use strict';
 
-// get the value of the search input
 
-// call the api with the proper parameters object
-
-// append the data link to the DOM
 
 // API KEY from Giphy
 const API_KEY = '47SoFht1UKvCbTBankcrDzcTocRDu5Xr';
+const BASE_URL = 'https://api.giphy.com/v1/gifs/search';
 
-/** Initialize the app */
-function initApp() {
 
-  /** add event listener submit button*/
-  $('form').on('submit', getGiphyData);
-
-  /** add event listener on delete button */
-  $('#delete').on('click', () => {
-    $('.giffs').html('');
-  })
-}
-
-/** Given a giphy data URL, appends the dom with the gif */
+/** Given a giphy gif url, set the src of an img tag and appends to dom */
 function updateDOM(giphyData) {
   const imageLink = giphyData.data[0].images.original.url;
-  $('.giffs').append(`<img src = ${imageLink}>`);
+  $('.gifs').append(`<img src = ${imageLink}>`);
 }
 
-/** When invoked, calls the giphy API and returns the URL for the gif */
-async function getGiphyData(evt) {
-  evt.preventDefault();
-  const searchTerm = $('input').val();
+/** When invoked, takes in a search parameter
+ *  calls the giphy API and returns the URL for the gif */
+async function getGiphyData(searchTerm) {
 
   const parameters = { params: { api_key: API_KEY, q: searchTerm, limit: 1 } };
 
-  const response = await axios.get('https://api.giphy.com/v1/gifs/search', parameters);
+  const response = await axios.get(BASE_URL, parameters);
 
-  updateDOM(response.data);
+  return response.data;
+
+}
+/** handles clicks on both form buttons */
+async function formClickHandler(e) {
+  e.preventDefault();
+
+  if (e.target.id === "submit") {
+    const searchTerm = $('input').val();
+    const data = await getGiphyData(searchTerm);
+    updateDOM(data);
+  }
+  if (e.target.id === "delete") {
+    $('.gifs').html('');
+  }
 }
 
-initApp();
+/**form event listener for both buttons */
+
+$('form').on('click', formClickHandler);
